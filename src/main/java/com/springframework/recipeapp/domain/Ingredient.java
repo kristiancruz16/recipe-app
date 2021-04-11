7e1046ec0c850f92ee0706f1c79a711fec3ff973
kristiancruz16 <kristian_m_cruz12@yahoo.com>
2021-04-11 18:30:12 +0800

    update

--- src/main/java/com/springframework/recipeapp/domain/Ingredient.java
+++ src/main/java/com/springframework/recipeapp/domain/Ingredient.java
@@ -11,6 +11,9 @@ public class Ingredient {
     private String description;
     private BigDecimal amount;
 
+    @OneToOne(fetch = FetchType.EAGER)
+    private UnitOfMeasure uom;
+
     @ManyToOne
     private Recipe recipe;
 
@@ -44,5 +47,13 @@ public class Ingredient {
 
     public void setRecipe(Recipe recipe) {
         this.recipe = recipe;
+    }
+
+    public UnitOfMeasure getUom() {
+        return uom;
+    }
+
+    public void setUom(UnitOfMeasure uom) {
+        this.uom = uom;
     }
 }
